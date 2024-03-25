9d9e013a1cc8aed31b4d5bd699a6d6777d79f797
thekvector <74088461+thekvector@users.noreply.github.com>
2024-03-25 10:59:07 -0400

    Add files via upload

---
+++ ai harness go/python/analysis.py
@@ -0,0 +1,41 @@
+# analysis.py
+import json
+import sys
+import numpy as np
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix, classification_report
+
+import os
+
+def analyze_data(file_name):
+    print(file_name)
+    input_filepath = os.path.join("..", "outputs", file_name)
+    print(input_filepath)
+    with open(input_filepath, 'r') as file:
+        data = json.load(file)
+
+    y_true = np.array([item['original_score'] for item in data])
+    y_scores = np.array([item['model_score'] for item in data])
+
+    threshold = 0.5
+    y_pred = np.where(y_scores >= threshold, 1, 0)
+
+    results = {
+        'accuracy': accuracy_score(y_true, y_pred),
+        'precision': precision_score(y_true, y_pred, zero_division=0),
+        'recall': recall_score(y_true, y_pred, zero_division=0),
+        'f1_score': f1_score(y_true, y_pred, zero_division=0),
+    }
+    output_dir = "../outputs"
+    results_file_name = input_filepath.replace('.json', '_results.json')
+    results_filepath = os.path.join(output_dir, results_file_name)
+    with open(results_filepath, 'w') as results_file:
+        json.dump(results, results_file, indent=4)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python main.py model_name input_file")
+        sys.exit(1)
+
+    file_name = sys.argv[1]
+    analyze_data(file_name)
