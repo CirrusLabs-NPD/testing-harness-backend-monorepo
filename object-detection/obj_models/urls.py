2b0f5888ca21721457f5d46aa2e4399dbc6a4336
Sabrina Thomas <sabrina.thomas@cirruslabs.io>
2024-05-15 12:34:34 -0400

    fixed my backend issues where it connects to my frontend

--- object-detection/obj_models/urls.py
+++ object-detection/obj_models/urls.py
@@ -14,4 +14,5 @@ urlpatterns = [
     path("dataset/", views.dataset, name="dataset"),
     path('models/', views.models, name='models'),
     path('run_model/<str:model_name>/', views.run_model, name='run_model'),
+    path('show_output/', views.show_output, name='show_output')
 ]
