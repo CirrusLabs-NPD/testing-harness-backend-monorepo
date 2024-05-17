84b10c3bd5ea903146ac861d140e0e3f1df3e8af
Aakash bhardwaj <112869168+Aakash-bhardwaj-cirruslabs@users.noreply.github.com>
2024-05-17 10:25:42 +0530

    Revert "Feature/readme.md"

--- object-detection/obj_models/views.py
+++ object-detection/obj_models/views.py
@@ -8,8 +8,6 @@ from .forms import ImageUploadForm
 from .models import Image
 import subprocess
 from django.http import HttpResponse
-from rest_framework.response import Response
-from rest_framework.decorators import api_view
 
 def upload_image(request):
     if request.method == 'POST':
@@ -55,33 +53,3 @@ def run_model(request, model_name):
     except Exception as e:
         # Handle any exceptions and display the error
         return HttpResponse(f"An error occurred: {str(e)}")
-
-# @csrf_exempt
-@api_view(['POST'])
-def show_output(request):
-    print(request.method)
-    if request.method == 'POST':
-        try:
-            print("hello world")# Execute the respective Python script based on the selected model
-            if request.data['model_name'] == 'Yolov8':
-                metrics = subprocess.run([sys.executable, 'yolo_m.py'], capture_output=True)
-            elif request.data['model_name'] == 'ResNet':
-                metrics = subprocess.run([sys.executable, 'yolo_m.py'], capture_output=True)
-            elif request.data['model_name'] == 'Fasterrcnn':
-                metrics = subprocess.run([sys.executable, 'detfasterrcnn.py'], capture_output=True)
-            else:
-                return Response({'error': 'Cannot display values'}, status=405)
-
-            # Check if the subprocess ran successfully
-            if metrics.returncode != 0:
-                raise subprocess.CalledProcessError(metrics.returncode, " ".join(metrics.args))
-
-            # Decode the output from bytes to string
-            metrics_output = metrics.stdout.decode()
-
-            # Display the output of the Python script
-            return Response({'metrics_output': metrics_output})
-        except Exception as e:
-            # Handle any exceptions and display the error
-            return Response({'error': 'Cannot display values'}, status=405)
-
